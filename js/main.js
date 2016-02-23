8d4e7992aecddc3f8bf95793b5f89bcc677aec75
Michelle <vand0432@algonquinlive.com>
2016-02-23 13:28:13 -0500

    fixed spacing before ++ operator

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var theLetter = prompt('Enter a letter.');
 var counter;
 
-for (counter = 0; counter < 10; counter ++) {
+for (counter = 0; counter < 10; counter++) {
   document.write(theLetter + '<br>');
 }
